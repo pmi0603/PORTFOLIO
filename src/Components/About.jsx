0db62c795766bd5efff4a4da54a2247c1a21ef97
pmi0603 <prashantmishra06032003@gmail.com>
2024-10-30 22:05:42 +0530

    Changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -33,7 +33,7 @@ function About() {
 
 <div id="mail">
     <FontAwesomeIcon icon={faEnvelope} id="mail_icon" />
-    <a href="mailto:pm622244@gmail.com" target="_blank" rel="noopener noreferrer">PrashantMishra</a>
+    <a href="mailto:prashantmishra06032003@gmail.com" target="_blank" rel="noopener noreferrer">PrashantMishra</a>
 </div>
     
 
